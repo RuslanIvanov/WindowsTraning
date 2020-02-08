0a622db1267c2aa2647ba2a74e6b40c787c554c0
RuslaIvanov <rus_iv@list.ru>
2020-02-08 19:32:50 +0300

    lab 8

---
+++ MVS2019/RuslanIvanov/lab8/lab8/templateVector.h
@@ -0,0 +1,26 @@
+#pragma once
+#include <iostream>
+template <typename T> void printParamCont(const T& t)
+{
+	std::cout << "\n------------------------------------------------";
+	std::cout << "\n name: " << typeid(t).name();
+	std::cout << "\n size: "<< t.size();
+	std::cout << "\n capacity: " << t.capacity();
+	std::cout << "\n max_size: " << t.max_size();
+	std::cout << "\n------------------------------------------------";
+}
+
+template <typename P> void printCont(const P& p)
+{
+	std::cout << "\nContainer '"<< typeid(p).name() << "' contains:";
+	if (p.empty())
+	{
+		std::cout << "  IS EMPTY!";
+		return;
+	}
+
+	for (size_t i = 0; i < p.size(); i++)
+	{
+		std::cout << "\n" << p[i];
+	}
+}
