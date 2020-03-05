8e2cea619383dd75c44f9b6c5fbe6672d42b7126
RuslanIvanov <rus_iv@list.ru>
2020-03-05 11:38:47 +0300

    ffffffff

--- MVS2019/RuslanIvanov/C++11/lab1_cpp11/lab1_cpp11/Header.h
+++ MVS2019/RuslanIvanov/C++11/lab1_cpp11/lab1_cpp11/Header.h
@@ -59,21 +59,25 @@ char mytolower_char(char st)
 
 template<typename T> void NegateAll( T& t) 
 {	
-	//for (auto& tt : t)
+	for (auto& tt : t)
 	{
-		std::transform(t.begin(), t.end(), t.begin(), mytolower_char);
+		std::cout << "\ntype is: " <<typeid(tt).name() << "";
+		//if (strcmp("double", typeid(tt).name()) == 0) 
+		//{		tt *= -1 ;		}
+
+		//std::transform(t.begin(), t.end(), t.begin(), mytolower_char);
 		//int i = atoi(tt);
 		//if (i != 0) { tt *= -1; }
 		//else
 		//tt = toupper(tt);*/
 		//if (tt == 0) { tt *= -1; }
-		//for (auto ttt : tt)
-		//{
+		//for(auto it = std::begin(tt); it != std::end(tt); ++it)
+		{
 			//if (ttt >= 'A' && ttt <= 'Z') { tolower(ttt); }
 			//else if (ttt >= 'a' && ttt <= 'z') { toupper(ttt); }
 			//else { ttt *= -1; }
-		//
+		}
 
-		//std::cout << "\n " << tt;
+		std::cout << "\n " << tt;
 	}
 }
