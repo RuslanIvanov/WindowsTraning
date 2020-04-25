ffe9f620c3bec03c510aa5a83eadea5d48ef9a25
RuslaIvanov <rus_iv@list.ru>
2020-04-25 23:53:02 +0300

    fff

--- MVS2019/RuslanIvanov/Parallel_C11-C20/Lab1_C11_C20/Lab1_C11_C20/threadsafe_stack.cpp
+++ MVS2019/RuslanIvanov/Parallel_C11-C20/Lab1_C11_C20/Lab1_C11_C20/threadsafe_stack.cpp
@@ -1,4 +1,4 @@
 
 using namespace std;
-#include "Header.h"
+//#include "Header.h"
 #include "threadsafe_stack.h"
--- MVS2019/RuslanIvanov/Parallel_C11-C20/Lab1_C11_C20/Lab1_C11_C20/threadsafe_stack.h
+++ MVS2019/RuslanIvanov/Parallel_C11-C20/Lab1_C11_C20/Lab1_C11_C20/threadsafe_stack.h
@@ -1,6 +1,17 @@
 #pragma once
 class threadsafe_stack
 {
-	vector<int> m_v;
+	//vector<int> m_v;
+	//void push(const int& p) // вставялть
+	//{
+	//}
+
+	//int pop() // выталкивать
+	//{
+
+	//}
+
+	//bool empty() {};
+	//size_t size() {};
 };
 
