964e869f8972740e0b4a42b9f264471ecc513679
RuslaIvanov <rus_iv@list.ru>
2020-05-07 00:17:43 +0300

    stack

--- MVS2019/RuslanIvanov/Parallel_C11-C20/Lab1_C11_C20/Lab1_C11_C20/threadsafe_stack.cpp
+++ MVS2019/RuslanIvanov/Parallel_C11-C20/Lab1_C11_C20/Lab1_C11_C20/threadsafe_stack.cpp
@@ -2,3 +2,42 @@
 using namespace std;
 //#include "Header.h"
 #include "threadsafe_stack.h"
+
+threadsafe_stack::threadsafe_stack(const threadsafe_stack& r)
+{
+
+}
+
+threadsafe_stack::threadsafe_stack(threadsafe_stack&&)
+{
+}
+
+threadsafe_stack& threadsafe_stack::operator=(threadsafe_stack&& st)
+{
+}
+threadsafe_stack& threadsafe_stack::operator=(threadsafe_stack& st)
+{
+}
+
+void threadsafe_stack::push(int)
+{
+	m_mut.lock();
+
+	m_mut.unlock();
+}
+
+void threadsafe_stack::top(int& r) const
+{
+	m_mut.lock_shared();
+
+	m_mut.unlock_shared();
+}
+
+void threadsafe_stack::pop(int& r) const
+{
+	m_mut.lock_shared();
+
+	m_mut.unlock_shared();
+}
+bool threadsafe_stack::empty() {}
+size_t threadsafe_stack::size() {}
--- MVS2019/RuslanIvanov/Parallel_C11-C20/Lab1_C11_C20/Lab1_C11_C20/threadsafe_stack.h
+++ MVS2019/RuslanIvanov/Parallel_C11-C20/Lab1_C11_C20/Lab1_C11_C20/threadsafe_stack.h
@@ -1,17 +1,26 @@
 #pragma once
+#include <vector>
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <shared_mutex>
 class threadsafe_stack
 {
-	//vector<int> m_v;
-	//void push(const int& p) // вставялть
-	//{
-	//}
+	mutable std::shared_mutex m_mut;
+	const size_t N = 10;
+	std::vector<int> m_v;
+public:
 
-	//int pop() // выталкивать
-	//{
+	threadsafe_stack() { m_v.resize(N); }
 
-	//}
-
-	//bool empty() {};
-	//size_t size() {};
+	threadsafe_stack(const threadsafe_stack&);
+	threadsafe_stack(threadsafe_stack&&);
+	threadsafe_stack& operator=(threadsafe_stack&& st);
+	threadsafe_stack& operator=(threadsafe_stack& st);
+	void push(int );
+	void top(int&) const;
+	void pop(int&) const;
+	bool empty();
+	size_t size();
 };
 
