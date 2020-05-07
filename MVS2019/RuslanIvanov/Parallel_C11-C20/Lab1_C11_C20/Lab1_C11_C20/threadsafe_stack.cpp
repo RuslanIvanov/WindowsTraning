adec4397baf8ce928b778d8ac92674a30249f210
RuslaIvanov <rus_iv@list.ru>
2020-05-07 22:45:09 +0300

    888

--- MVS2019/RuslanIvanov/Parallel_C11-C20/Lab1_C11_C20/Lab1_C11_C20/threadsafe_stack.cpp
+++ MVS2019/RuslanIvanov/Parallel_C11-C20/Lab1_C11_C20/Lab1_C11_C20/threadsafe_stack.cpp
@@ -19,24 +19,30 @@ threadsafe_stack& threadsafe_stack::operator=(threadsafe_stack& st)
 {
 }
 
-void threadsafe_stack::push(int)
-{
+void threadsafe_stack::push(int val)
+{//Вставляет элемент на верх.
 	m_mut.lock();
-
+	m_v.push_back(val);
 	m_mut.unlock();
 }
 
 void threadsafe_stack::top(int& r) const
-{
+{//Доступ к верхнему элементу 
 	m_mut.lock_shared();
 
 	m_mut.unlock_shared();
 }
 
 void threadsafe_stack::pop(int& r) const
-{
+{//Удаляет верхний элемент. 
 	m_mut.lock_shared();
-
+	if (m_v.size())
+	{
+		val = data_[0];
+		data_.erase(data_.begin() + 0);
+		return true;
+	}
+	return false;
 	m_mut.unlock_shared();
 }
 bool threadsafe_stack::empty() {}
